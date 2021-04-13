5ac02d7518f6ca7908b1adca265d5116b4a2b931
Fatih Cevizoğlu <fatihcevizoglu@hotmail.com>
2021-04-13 14:49:27 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+   public class Contact
+    {
+        [Key]
+        public int ContacID { get; set; }
+        
+        [StringLength(50)]
+        public string UserName { get; set; }
+        [StringLength(50)]
+        public string UserMail { get; set; }
+        [StringLength(50)]
+        public string Subject { get; set; }
+        
+        public string Message { get; set; }
+
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+
+    }
+}
